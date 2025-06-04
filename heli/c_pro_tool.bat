d5a373b78ea06a3fa58a25a2a8996f9a6afc7f51
takuto0318 <143781359+takuto0318@users.noreply.github.com>
2025-06-04 18:30:10 +0900

    Add files via upload

---
+++ heli/c_pro_tool.bat
@@ -0,0 +1,4 @@
+@echo off
+CALL "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\Tools \VsDevCmd.bat"
+set PATH=%PATH%;"C:\Program Files (x86)\TeraPad"
+c:\windows\system32\cmd.exe
